81c1b84151f5d479e4a52e1afbae2a013ac82c1c
hurielreichel <huriel.reichel@protonmail.com>
2021-10-30 20:07:47 +0200

    more pages wg gesucht

--- zimmer-finden.py
+++ zimmer-finden.py
@@ -7,27 +7,25 @@ First Experiment with WG-Gesucht website
 
 Missing : other related websites
 """
+if __name__ == "__main__":
 
-pm_argparse = argparse.ArgumentParser()
+    pm_argparse = argparse.ArgumentParser()
 
-# argument and parameter directive #
-pm_argparse.add_argument( '--startDate' , type=str  , help='date of desired move in DD.MM.YEAR format' )
-pm_argparse.add_argument( '--price',  type=int , help='maximum price')
-pm_argparse.add_argument( '--poi', type=int, help='location of point of interest')
+    # argument and parameter directive #
+    pm_argparse.add_argument( '--startDate' , type=str  , help='date of desired move in DD.MM.YEAR format' )
+    pm_argparse.add_argument( '--price',  type=int , help='maximum price')
+    pm_argparse.add_argument( '--poi', type=int, help='location of point of interest')
 
-# read argument and parameters #
-pm_args = pm_argparse.parse_args()
+    # read argument and parameters #
+    pm_args = pm_argparse.parse_args()
 
-# wg-gesucht websites (WG Query)
-url1 = "https://www.wg-gesucht.de/wg-zimmer-und-1-zimmer-wohnungen-und-wohnungen-und-haeuser-in-Munster.91.0+1+2+3.1.0.html?offer_filter=1&city_id=91&noDeact=1&categories%5B%5D=0&categories%5B%5D=1&categories%5B%5D=2&categories%5B%5D=3&rent_types%5B%5D=2%2C1"
-
-url2 = "https://www.wg-gesucht.de/wg-zimmer-und-1-zimmer-wohnungen-und-wohnungen-und-haeuser-in-Munster.91.0+1+2+3.1.1.html?categories=0%2C1%2C2%2C3&city_id=91&rent_types%5B0%5D=2&rent_types%5B1%5D=1&noDeact=1&img=1"
-
-def wggesucht(url):
+    # wg-gesucht websites (WG Query)
+    url = "https://www.wg-gesucht.de/wg-zimmer-in-Munster.91.0.1.0.html"
+    url = "https://www.wg-gesucht.de/wg-zimmer-und-1-zimmer-wohnungen-und-wohnungen-und-haeuser-in-Munster.91.0+1+2+3.1.0.html?offer_filter=1&city_id=91&noDeact=1&categories%5B%5D=0&categories%5B%5D=1&categories%5B%5D=2&categories%5B%5D=3&rent_types%5B%5D=2%2C1"
 
     response = requests.get(url)
     soup = BeautifulSoup(response.content, "lxml")
-    
+
     # main cards for iteration
     announce_card = soup.find_all('div', class_= 'col-sm-8 card_body')
     span_card = soup.find_all('div', class_= 'col-xs-11')
@@ -54,15 +52,14 @@ def wggesucht(url):
     if (len(link) == len(price) == len(info)) == False :
 
         link = link[1: ]
-        price = price[1: ]
+        info = info[1: ]
 
         print('\n add removed \n')
 
-    print('\n', link, '\n', price, '\n', info, '\n')
+    print(link, '\n', price, '\n', info)
 
-wggesucht(url1)
-wggesucht(url2)
-
+    # query for 'ein Zimmer Wohnung' #
+    # process more pages  - one page enough ?#
     # remove old posts
     # option : moving day
     # option : price
