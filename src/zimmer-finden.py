ac62e60d0b8f83266373c24a01400ecced7945e9
hurielreichel <huriel.reichel@protonmail.com>
2021-11-15 01:52:14 +0100

    idw - not optimal yet

--- src/zimmer-finden.py
+++ src/zimmer-finden.py
@@ -19,14 +19,21 @@ import requests
 import argparse
 import datetime
 import folium
-import pyproj
 import webbrowser
+import math
 import pandas as pd
 import time as tp
+import numpy as np
+import matplotlib.pyplot as plt #to be removed
 from bs4 import BeautifulSoup
 from geopy.extra.rate_limiter import RateLimiter
 from geopy.geocoders import Nominatim
-from scipy.spatial.distance import cdist
+from pyproj import Transformer
+from osgeo import gdal
+from osgeo import gdal_array
+from osgeo import osr
+from matplotlib.colors import ListedColormap
+from branca.element import Template, MacroElement
 
 # argument parser
 pm_argparse = argparse.ArgumentParser()
@@ -44,6 +51,12 @@ pm_args = pm_argparse.parse_args()
 url1 = "https://www.wg-gesucht.de/wg-zimmer-und-1-zimmer-wohnungen-und-wohnungen-und-haeuser-in-Munster.91.0+1+2+3.1.0.html?offer_filter=1&city_id=91&noDeact=1&categories%5B%5D=0&categories%5B%5D=1&categories%5B%5D=2&categories%5B%5D=3&rent_types%5B%5D=2%2C1"
 
 url2 = "https://www.wg-gesucht.de/wg-zimmer-und-1-zimmer-wohnungen-und-wohnungen-und-haeuser-in-Munster.91.0+1+2+3.1.1.html?categories=0%2C1%2C2%2C3&city_id=91&rent_types%5B0%5D=2&rent_types%5B1%5D=1&noDeact=1&img=1"
+
+def dist(x, y, x0, y0) :
+
+    d = math.sqrt( (x - x0) ** 2 + (y - y0) ** 2)
+
+    return (d)
 
 def wggesucht(url):
 
@@ -111,10 +124,28 @@ def wggesucht(url):
     
     return( df )
 
+def distance_matrix(x0, y0, x1, y1):
+    obs = np.vstack((x0, y0)).T
+    interp = np.vstack((x1, y1)).T
 
-# end of function
+    d0 = np.subtract.outer(obs[:,0], interp[:,0])
+    d1 = np.subtract.outer(obs[:,1], interp[:,1])
 
+    return np.hypot(d0, d1)
 
+def idw(x, y, z, xi, yi):
+    dist = distance_matrix(x,y, xi,yi)
+
+    weights = dist ** -4
+    weights /= weights.sum(axis=0)
+
+    zi = np.dot(weights.T, z)
+
+    return zi
+
+# end of functions
+
+# web scrapping
 df_p1 = wggesucht(url1)
 df_p2 = wggesucht(url2)
 df_p12 = pd.concat([df_p1, df_p2])
@@ -128,7 +159,7 @@ if pm_args.date is not None:
 
     given_time = tp.mktime(given_time.timetuple())
   
-    df_p12 = df_p12[df_p12['time'] >= given_time]
+    #df_p12 = df_p12[df_p12['time'] >= given_time] #if you want to exclude points with later moving dates
 
 # price option
 if pm_args.price is not None:
@@ -140,7 +171,6 @@ if pm_args.price is not None:
     df_p12 = df_p12[df_p12['price'] <= given_price]
 
 # geocoding
-
 print(" \n geocoding... \n ")
 locator = Nominatim(user_agent="myGeocoder")
 geocode = RateLimiter(locator.geocode, min_delay_seconds=1)
@@ -191,12 +221,15 @@ df_p12['point'] = df_p12['location'].apply(lambda loc: tuple(loc.point) if loc e
 df_p12 = df_p12.dropna(subset = ['point'])
 df_p12[['latitude', 'longitude', 'altitude']] = pd.DataFrame(df_p12['point'].tolist(), index=df_p12.index)
 
-# folium interactive map
+# Optimal location
 locations = df_p12[['latitude', 'longitude']]
 locationlist = locations.values.tolist()
 links = list(df_p12['link'])
 prices = list(df_p12['price'])
 
+transformer = Transformer.from_crs("epsg:4326", "epsg:32632")
+lon, lat = transformer.transform(df_p12.latitude, df_p12.longitude)
+    
 # distance to point option
 if pm_args.poi is not None:
 
@@ -212,52 +245,209 @@ if pm_args.poi is not None:
         print('Address invalid or not found')
 
     df_poi[['latitude', 'longitude', 'altitude']] = pd.DataFrame(df_poi['point'].tolist(), index=df_poi.index)
-    latlong = [df_poi.latitude, df_poi.longitude]
 
-    wgs84 = pyproj.Proj(projparams = 'epsg:4326')
-    utm = pyproj.Proj(projparams = 'epsg:32632')
-
-    lon, lat = pyproj.transform(wgs84, utm, df_p12.latitude, df_p12.longitude)
-    lon_poi, lat_poi = pyproj.transform(wgs84, utm, df_poi.latitude, df_poi.longitude)
-
-
-    df_p12['lon'] = lon
-    df_p12['lat'] = lat
-    df_poi['lat'] = lat_poi
-    df_poi['lon'] = lon_poi
+    lon_poi, lat_poi = transformer.transform(df_poi.latitude, df_poi.longitude)
 
     # calculate distance
-    m = cdist(df_p12[['lon', 'lat']], df_poi[['lon', 'lat']], 'euclidean')
-    df_p12['dist'] = m
+    d = []
+
+    for i in range(len(lon)):
+
+        d.append(dist(lon[i], lat[i], lat_poi, lon_poi))
+
+    df_p12['dist'] = d
 
 # calculate optimal place index
 if pm_args.date is not None and pm_args.poi is not None:
 
-    df_p12['time_w'] = df_p12['time'] - given_time
-    df_p12['index'] = df_p12['price'] + df_p12['dist']/1000 + df_p12['time_w']
+    df_p12['time_w'] = df_p12['time'].astype(float) - given_time
+    df_p12['index'] = df_p12['price'].astype(float) + df_p12['dist'].astype(float) + df_p12['time_w'].astype(float)
 
 elif pm_args.date is not None and pm_args.poi is None:
 
-    df_p12['time_w'] = df_p12['time'] - given_time
-    df_p12['index'] = df_p12['price'] + df_p12['time_w']
+    df_p12['time_w'] = df_p12['time'].astype(float) - given_time
+    df_p12['index'] = df_p12['price'].astype(int) + df_p12['time_w'].astype(float)
+
+elif pm_args.date is None and pm_args.poi is not None:
+
+    df_p12['index'] = df_p12['price'].astype(int) + df_p12['dist'].astype(float)
 
 else:
 
-    df_p12['index'] = df_p12['price']
+    df_p12['index'] = df_p12['price'].astype(int)
 
+df_p12['index'] =  (( df_p12['index'] - df_p12['index'].min() ) / ( df_p12['index'].max() - df_p12['index'].min() ))
 #interpolate index for best place
 
+# sampling grid
+xmin = int(394000) 
+width = int(30000)
+xmax = int(xmin + width)
+ymin = int(5737000) 
+height = int(32000) 
+ymax = int(ymin + height)
+
+x = lon
+y = lat
+z = list(df_p12['index'])
+
+# grid resolution
+nx = int(width/200)
+ny = int(height/200)
+
+xi = np.linspace(xmin, xmax, nx)
+yi = np.linspace(ymin, ymax, ny)
+xi, yi = np.meshgrid(xi, yi)
+xi, yi = xi.flatten(), yi.flatten()
+
+# Compute Inverse Distance Weighting
+grid1 = idw(x,y,z,xi,yi)
+grid1 = grid1.reshape((ny, nx))
+
+# define and export raster - should be optional
+
+nrows,ncols = np.shape(grid1)
+geotransform=(xmin,nx,0,ymax,0,-ny)
+filename = pm_args.output + str(int(tp.time())) + '-wohn.tif'
+output_raster = gdal.GetDriverByName('GTiff').Create(filename,ncols, nrows, 1, gdal.GDT_Float32)
+output_raster.SetGeoTransform(geotransform)  
+srs = osr.SpatialReference()                 
+srs.ImportFromEPSG(32632)
+output_raster.SetProjection( srs.ExportToWkt() )    
+output_raster.GetRasterBand(1).WriteArray(grid1)  
+output_raster.FlushCache()
+
+rst = gdal.Warp('', output_raster, dstSRS='EPSG:4326', format='VRT',
+               outputType=gdal.GDT_Float32)
+geotransform = rst.GetGeoTransform()
+cols = rst.RasterXSize 
+rows = rst.RasterYSize 
+xmin=geotransform[0]
+ymax=geotransform[3]
+xmax=xmin+cols*geotransform[1]
+ymin=ymax+rows*geotransform[5]
+
+# folium interactive map
 ### make this popup look better ###
 map = folium.Map(location = [51.95371107628213, 7.628077552663438], zoom_start = 13)
+
 for point in range(0, len(locationlist)):
-    folium.Marker(locationlist[point], popup=[links[point],prices[point]]).add_to(map)
+
+    folium.Marker(locationlist[point],
+                  popup=folium.Popup(links[point] + '<br>' + 'Price : €' + prices[point], max_width = 3000),
+                  icon=folium.Icon(color="blue",icon="home", prefix='fa')).add_to(map)
+
+pal = ListedColormap(["darkred", "red", "seashell", "lightgreen", "forestgreen"])
+
+folium.raster_layers.ImageOverlay(
+    image=grid1,
+    opacity = 0.5,
+    bounds =[[ymin, xmin], [ymax, xmax]],
+    colormap=pal).add_to(map)
+
+# legend
+template = """
+{% macro html(this, kwargs) %}
+
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>WOHN - Find your home in Münster</title>
+  <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+
+  <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+  
+  <script>
+  $( function() {
+    $( "#maplegend" ).draggable({
+                    start: function (event, ui) {
+                        $(this).css({
+                            right: "auto",
+                            top: "auto",
+                            bottom: "auto"
+                        });
+                    }
+                });
+});
+
+  </script>
+</head>
+<body>
+
+ 
+<div id='maplegend' class='maplegend' 
+    style='position: absolute; z-index:9999; border:2px solid grey; background-color:rgba(255, 255, 255, 0.8);
+     border-radius:6px; padding: 10px; font-size:14px; right: 20px; bottom: 20px;'>
+     
+<div class='legend-title'>Index of Suitability</div>
+<div class='legend-scale'>
+  <ul class='legend-labels'>
+    <li><span style='background:darkred;opacity:0.7;'></span>Very Bad</li>
+    <li><span style='background:red;opacity:0.7;'></span>Bad</li>
+    <li><span style='background:seashell;opacity:0.7;'></span>Medium</li>
+    <li><span style='background:lightgreen;opacity:0.7;'></span>Good</li>
+    <li><span style='background:forestgreen;opacity:0.7;'></span>Very Good</li>
+  </ul>
+</div>
+</div>
+ 
+</body>
+</html>
+
+<style type='text/css'>
+  .maplegend .legend-title {
+    text-align: left;
+    margin-bottom: 5px;
+    font-weight: bold;
+    font-size: 90%;
+    }
+  .maplegend .legend-scale ul {
+    margin: 0;
+    margin-bottom: 5px;
+    padding: 0;
+    float: left;
+    list-style: none;
+    }
+  .maplegend .legend-scale ul li {
+    font-size: 80%;
+    list-style: none;
+    margin-left: 0;
+    line-height: 18px;
+    margin-bottom: 2px;
+    }
+  .maplegend ul.legend-labels li span {
+    display: block;
+    float: left;
+    height: 16px;
+    width: 30px;
+    margin-right: 5px;
+    margin-left: 0;
+    border: 1px solid #999;
+    }
+  .maplegend .legend-source {
+    font-size: 80%;
+    color: #777;
+    clear: both;
+    }
+  .maplegend a {
+    color: #777;
+    }
+</style>
+{% endmacro %}"""
+
+macro = MacroElement()
+macro._template = Template(template)
+
+map.get_root().add_child(macro)
 
 out_path = pm_args.output + 'wohn.html'
 map.save(out_path)
-
 webbrowser.open(out_path)
 
-#interpolates value of distance - distance to public transport
 #plot surface (quartile - (very far, far, reasonable, close, very close))
 #colours for price
 #association with links
+#men or woman desired for room
